81b9e687fd91582d9edb3983373bf75782b703a5
Sinis2101 <sinisterra2101@gmail.com>
2021-10-14 15:42:36 -0500

    Program now runs and the main window is opened.

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,19 +1,36 @@
 package ui;
 
+import controller.DatabaseController;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
-import model.Player;
-
-import java.time.LocalDate;
+import model.Database;
 
 public class Main extends Application {
 
-    public static void main(String[] args) {
+    private final DatabaseController databaseController;
+    private Database database;
 
+    public Main() {
+        database = new Database();
+        databaseController = new DatabaseController(database);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("scenes/main.fxml"));
+        fxmlLoader.setController(databaseController);
+        Parent root = fxmlLoader.load();
+        primaryStage.setTitle("Basketball Database");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
